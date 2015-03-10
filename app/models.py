fe0c69c955494a8fddedc329b555c08c26fe06b3
David Bain <david@alteroo.com>
2015-03-10 14:09:35 +0000

    added a Flask-Login compatible model

--- app/models.py
+++ app/models.py
@@ -4,3 +4,21 @@ class Myprofile(db.Model):
     first_name = db.Column(db.String(80), unique=True)     
     last_name = db.Column(db.String(80), unique=True)     
     nickname = db.Column(db.String(80), unique=True)     
+    
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
