c2f0158522f932a671660ff29d390d947c5aed7b
David Bain <david@alteroo.com>
2015-03-10 14:10:50 +0000

    removed nickname for now

--- app/models.py
+++ app/models.py
@@ -2,8 +2,7 @@ from . import db
 class Myprofile(db.Model):     
     id = db.Column(db.Integer, primary_key=True)     
     first_name = db.Column(db.String(80), unique=True)     
-    last_name = db.Column(db.String(80), unique=True)     
-    nickname = db.Column(db.String(80), unique=True)     
+    last_name = db.Column(db.String(80), unique=True)    
     
     def is_authenticated(self):
         return True
