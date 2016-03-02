71c389aae55bae2a9190eebede3d89fdf06c0608
maschad <chad.nehemiah94@gmail.com>
2016-03-02 14:03:09 -0500

    Silly error with uri

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ app.config['SECRET_KEY'] = "this is a super secure key"
 app.config['OPENID_PROVIDERS'] = COMMON_PROVIDERS
 
 # remember to change to heroku's database
-app.config['SQLALCHEMY_DATABASE_URI'] = postgres://zqexxmeqqdemiv:fwE8TDtVwxklzYQk3ojJ6kwEWQ@ec2-54-83-0-187.compute-1.amazonaws.com:5432/de3sv5dg3upfl4
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zqexxmeqqdemiv:fwE8TDtVwxklzYQk3ojJ6kwEWQ@ec2-54-83-0-187.compute-1.amazonaws.com:5432/de3sv5dg3upfl4'
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
