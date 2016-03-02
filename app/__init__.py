cfca122f6cc065eb8e7c6e94f7d466929597acdc
maschad <chad.nehemiah94@gmail.com>
2016-03-02 05:28:33 -0500

    think it works

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,26 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID, COMMON_PROVIDERS
 
+import os
+import urlparse
+
+from sqlalchemy.dialects.postgresql import psycopg2
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "this is a super secure key"
 app.config['OPENID_PROVIDERS'] = COMMON_PROVIDERS
+
 # remember to change to heroku's database
-# @TODO: app.config['SQLALCHEMY_DATABASE_URI'] = 'heroku'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://project:admin@localhost/project'
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
