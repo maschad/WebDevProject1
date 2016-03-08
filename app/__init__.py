2eb96bbb2b8c8b052fef9c359b88db7425fd8982
maschad <chad.nehemiah94@gmail.com>
2016-03-08 18:12:01 -0500

    heroku deployable

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "this is a super secure key"
 
 # remember to change to heroku's database
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zqexxmeqqdemiv:fwE8TDtVwxklzYQk3ojJ6kwEWQ@ec2-54-83-0-187.compute-1.amazonaws.com:5432/de3sv5dg3upfl4'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://project:admin@localhost/project'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zqexxmeqqdemiv:fwE8TDtVwxklzYQk3ojJ6kwEWQ@ec2-54-83-0-187.compute-1.amazonaws.com:5432/de3sv5dg3upfl4'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://project:admin@localhost/project'
 db = SQLAlchemy(app)
 
 
