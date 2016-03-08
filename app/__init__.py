cb54134fb2ae919a93d90af437ac0439f894eac4
maschad <chad.nehemiah94@gmail.com>
2016-03-08 15:59:47 -0500

    Still trying to get it to work on heroke

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,17 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.login import LoginManager
-from flask.ext.openid import OpenID, COMMON_PROVIDERS
 
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "this is a super secure key"
-app.config['OPENID_PROVIDERS'] = COMMON_PROVIDERS
 
 # remember to change to heroku's database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zqexxmeqqdemiv:fwE8TDtVwxklzYQk3ojJ6kwEWQ@ec2-54-83-0-187.compute-1.amazonaws.com:5432/de3sv5dg3upfl4'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://project:admin@localhost/project'
 db = SQLAlchemy(app)
-lm = LoginManager()
-lm.init_app(app)
-oid = OpenID(app,'/tmp')
+
 
 from app import views
 from app.models import Myprofile
