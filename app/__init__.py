3b31f625580e9237e48176bc1ccfc405a74f7fe4
maschad <chad.nehemiah94@gmail.com>
2016-03-08 19:50:18 -0500

    Heroku stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-
+import sys
+import logging
 
 
 app = Flask(__name__)
@@ -11,6 +12,8 @@ app.config['SECRET_KEY'] = "this is a super secure key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zqexxmeqqdemiv:fwE8TDtVwxklzYQk3ojJ6kwEWQ@ec2-54-83-0-187.compute-1.amazonaws.com:5432/de3sv5dg3upfl4'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://project:admin@localhost/project'
 db = SQLAlchemy(app)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 
 from app import views
