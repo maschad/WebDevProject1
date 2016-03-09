196fbd4cd3af88a2b9cffde3270765366f1f86ec
maschad <chad.nehemiah94@gmail.com>
2016-03-08 20:20:55 -0500

    def fixed adding

--- app/forms.py
+++ app/forms.py
@@ -1,22 +1,20 @@
-from wtforms import Form,SelectField,TextField,SubmitField,PasswordField, validators
+from wtforms import Form,SelectField,TextField,SubmitField,PasswordField,IntegerField,validators
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 class ProfileForm(Form):
-     username = TextField('Username', [validators.Length(min=2, max=25)])
+     username = TextField('Username', validators=[validators.username_validator()])
      first_name = TextField('First Name', [validators.Required()])
      last_name = TextField('Last Name', [validators.Required()])
-     age = TextField('Age',[validators.Required()])
+     age = IntegerField('Age',[validators.Required()])
      sex = SelectField('Sex',choices=[('M','Male'),('F','Female')])
      image = FileField('Image File',validators =[FileRequired(),FileAllowed(['jpg', 'png'], 'Images only!')])
      submit = SubmitField("Send")
 
-
-class LoginForm(Form):
-     username = TextField('Username', validators.Required())
-     password= PasswordField('Password', validators.Required())
-
-class LoginForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
+def username_validator(form , field):
+    user = db.session.query(User).filter_by(username=field.data).first()
+    if user != None:
+        field.errors.append('Username already taken')
+        return False
+    return True
